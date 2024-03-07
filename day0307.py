4cf2dd6c57810c69d0650f2356d4d8a9ee7c877c
soyeongpark2090 <syp4546@gmail.com>
2024-03-07 23:29:40 +0900

    오늘의 코드카타 1~5

--- day0307.py
+++ day0307.py
@@ -1,39 +1,66 @@
-# def solution(price):
-#     answer = 0
-#     if price >= 500000:
-#         answer = 0.8*price
-#     elif price >= 300000:
-#         answer = 0.9*price
-#     elif price >= 100000:
-#         answer = 0.95*price
-#     else:               #빠진 조건 넣음 => 정답
-#         answer = price
-#     return int(answer)
-# # #오류=>if elif elif 조건 충돌나지 않게 해줘야 함 + 빠진 조건 없는지!
+def solution(price):
+    answer = 0
+    if price >= 500000:
+        answer = 0.8*price
+    elif price >= 300000:
+        answer = 0.9*price
+    elif price >= 100000:
+        answer = 0.95*price
+    else:  # 빠진 조건 넣음 => 정답
+        answer = price
+    return int(answer)
+# 오류=>if elif elif 조건 충돌나지 않게 해줘야 함 + 빠진 조건 없는지!
+# price 자체는 값을 가지므로 answer로 변수 준 경우와는 필요 없음
 
 
-# def solution(price):
-#     if price >= 500000:
-#         price = 0.8*price
-#     elif price >= 300000:
-#         price = 0.9*price
-#     elif price >= 100000:
-#         price = 0.95*price
-#     return int(price)
-# # #정답
+def solution(price):
+    if price >= 500000:
+        price = 0.8*price
+    elif price >= 300000:
+        price = 0.9*price
+    elif price >= 100000:
+        price = 0.95*price
+    return int(price)
+# 정답
 
 
-# print(solution(580000))
+print(solution(580000))
+
+# 세균증식
 
 
+def solution(n, t):
+    return (2**t)*n
+
+
+# 삼각형의 완성조건
+def solution(sides):
+    if max(sides) < sum(sides)-max(sides):
+        return 1
+    return 2
+
+
+# 중앙값 구하기
+def solution(array):
+    array.sort()
+    for i in range(len(array)):
+        if i == len(array)//2:
+            return array[i]
+
+
+# 짝수는 싫어요
 def solution(n):
-    for i in range(n):
-        ans = '#'*n
-    print(ans)
+    answer = []
+    for i in range(1, n+1):
+        if i % 2 != 0:
+            answer.append(i)
+    return answer
 
 
-solution(n)
-
-
-
-pan = 
+# 순서쌍의 개수
+def solution(n):
+    answer = []
+    for i in range(1, n+1):
+        if n % i == 0:
+            answer.append((i, n//i))
+    return len(answer)
--- pp2.py
+++ pp2.py
@@ -1,30 +1,20 @@
-# T = int(input())
+# swea 패턴마디의 길이
+T = int(input())
 
-# for test_case in range(1, T+1):
-#     string = list(input())
-
-#     for i in range(len(string)):
-#         for j in range(1, 11):
-#             if string[0][0:j+1] == string[0][j+1:2*j+3]:
-#                 print(f'#{test_case} {j+1}')
-
-
-# T = int(input())
-
-# for test_case in range(1, T+1):
-#     string = input()
-#     length = len(string)
-#     for j in range(1, length+1):
-#         if string[:j] == string[j:2*j]:
-#             print(f'#{test_case} {j}')
-#             break
+for test_case in range(1, T+1):
+    string = input()
+    length = len(string)  # 마디 최대길이가 안 주어진 경우
+    for j in range(1, length+1):
+        if string[:j] == string[j:2*j]:
+            print(f'#{test_case} {j}')
+            break
 
 
 T = int(input())
 
 for test_case in range(1, T+1):
     string = input()
-    for j in range(1, 11):
+    for j in range(1, 11):  # 마디 최대길이가 10으로 주어짐
         if string[:j] == string[j:2*j]:
             print(f'#{test_case} {j}')
             break
