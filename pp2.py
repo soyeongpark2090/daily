43c37ce282d113ee4af73d3edfdb777a7d4d42ea
soyeongpark2090 <syp4546@gmail.com>
2024-03-11 23:58:28 +0900

    N과 M(1), 연산자 끼워넣기, 코드카타

--- pp2.py
+++ pp2.py
@@ -1,13 +1,13 @@
 # swea 패턴마디의 길이
-T = int(input())
+# T = int(input())
 
-for test_case in range(1, T+1):
-    string = input()
-    length = len(string)  # 마디 최대길이가 안 주어진 경우
-    for j in range(1, length+1):
-        if string[:j] == string[j:2*j]:
-            print(f'#{test_case} {j}')
-            break
+# for test_case in range(1, T+1):
+#     string = input()
+#     length = len(string)  # 마디 최대길이가 안 주어진 경우
+#     for j in range(1, length+1):
+#         if string[:j] == string[j:2*j]:
+#             print(f'#{test_case} {j}')
+#             break
 
 
 T = int(input())
